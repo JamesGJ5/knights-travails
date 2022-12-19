8d9f4353903cb3a549f57f1a0322c0c6ae0f7683
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-19 12:32:55 +0000

    Initialise queue for breadth-first traversal as deque

--- src/modules/board.js
+++ src/modules/board.js
@@ -1,6 +1,10 @@
+import Deque from '../../node_modules/collections/deque';
+
 export default class Board {
   static knightMoves(startCoords, endCoords) {
+    // TODO: make sure startCoords and endCoords are in the correct format
     const prev = Board.makePrev(endCoords);
+    const queue = new Deque([endCoords]);
   }
 
   static makePrev(endCoords) {
