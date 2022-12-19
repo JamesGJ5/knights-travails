532e9801cbe011cd7da281118ca45dd5c9848b99
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-19 13:58:10 +0000

    Change parameter name for clarity

--- src/modules/board.js
+++ src/modules/board.js
@@ -5,18 +5,18 @@ import Deque from '../../node_modules/collections/deque';
 export default class Board {
   static knightMoves(startCoords, endCoords) {
     // TODO: make sure startCoords and endCoords are in the correct format
-    const prev = Board.makePrev(endCoords);
+    const prevGrid = Board.makePrev(endCoords);
     const deque = new Deque([endCoords]);
-    Board.doTraversal(prev, deque, startCoords);
+    Board.doTraversal(prevGrid, deque, startCoords);
   }
 
-  static doTraversal(prev, deque, knightStartCoords) {
+  static doTraversal(prevGrid, deque, knightStartCoords) {
     while (deque.length > 0) {
       const currentCoords = deque.shift();
-      const moveList = Board.getMoves(currentCoords, prev);
+      const moveList = Board.getMoves(currentCoords, prevGrid);
       for (let i = 0; i < moveList.length; i += 1) {
         const move = moveList[i];
-        Board.logPrevious(move, currentCoords, prev);
+        Board.logPrevious(move, currentCoords, prevGrid);
         if (move === knightStartCoords) {
           return;
         }
@@ -25,16 +25,16 @@ export default class Board {
   }
 
   static makePrev(endCoords) {
-    const prev = [...Array(8)].map(() => Array(8).fill(null));
-    Board.logPrevious(endCoords, endCoords, prev);
-    return prev;
+    const prevGrid = [...Array(8)].map(() => Array(8).fill(null));
+    Board.logPrevious(endCoords, endCoords, prevGrid);
+    return prevGrid;
   }
 
-  static logPrevious(coords, prevCoords, prev) {
-    prev[coords[0]][coords[1]] = prevCoords;
+  static logPrevious(coords, prevCoords, prevGrid) {
+    prevGrid[coords[0]][coords[1]] = prevCoords;
   }
 
-  static getMoves(coords, prev) {
+  static getMoves(coords, prevGrid) {
     // Returns a list of unvisited spaces that can be moved to by the knight in
     // one move from coords
     const i = coords[0];
@@ -44,11 +44,11 @@ export default class Board {
       for (let largeIncrement = -2; largeIncrement <= 2; largeIncrement += 4) {
         // TODO: combine the below into one loop
         const move1 = [i + smallIncrement, j + largeIncrement];
-        if (Board.checkMoveValidity(move1, prev)) {
+        if (Board.checkMoveValidity(move1, prevGrid)) {
           moves.push(move1);
         }
         const move2 = [i + largeIncrement, j + smallIncrement];
-        if (Board.checkMoveValidity(move2, prev)) {
+        if (Board.checkMoveValidity(move2, prevGrid)) {
           moves.push(move2);
         }
       }
@@ -56,11 +56,11 @@ export default class Board {
     return moves;
   }
 
-  static checkMoveValidity(coords, prev) {
+  static checkMoveValidity(coords, prevGrid) {
     // TODO: consider splitting the below into two methods
     const i = coords[0];
     const j = coords[1];
-    if (i < 0 || i > 7 || j < 0 || j > 7 || prev[i][j] !== null) {
+    if (i < 0 || i > 7 || j < 0 || j > 7 || prevGrid[i][j] !== null) {
       return false;
     }
     return true;
